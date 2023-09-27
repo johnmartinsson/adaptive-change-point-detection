8449c767ac5616452deb2a6e5ae9af88d51d3938
John Martinsson <john.martinsson@gmail.com>
2023-09-27 15:32:25 +0200

    Updates code.

---
+++ generate_scaper_data_ME.sh
@@ -0,0 +1,4 @@
+python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=40
+python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=30
+python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=20
+python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=10
