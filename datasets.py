e93a9073dc6251f86139d567a3e38e1682242815
John Martinsson <john.martinsson@gmail.com>
2023-10-06 13:43:31 +0200

    Updates the datasets methods;

--- datasets.py
+++ datasets.py
@@ -2,8 +2,58 @@ import os
 import glob
 import numpy as np
 
-def load_timings_and_embeddings(file_path, embedding_dim=1024):
-    timings_and_embeddings = []
+
+def load_pos_ref(ref_path):
+    pos_ref = []
+    
+    with open(ref_path, 'r') as f:
+        ls = f.readlines()
+        for l in ls:
+            ds = l.split('\t')
+            start_time = float(ds[0])
+            end_time = float(ds[1])
+            pos_ref.append((start_time, end_time))
+    return pos_ref
+
+def compute_neg_from_pos(pos_ref, soundscape_length):
+    pos_ref = sorted(pos_ref, key=lambda x: x[0])
+
+    neg_ref = []
+    prev_pos_end_time   = 0
+    for (curr_pos_start_time, curr_pos_end_time) in pos_ref:
+        neg_start_time = prev_pos_end_time
+        neg_end_time   = curr_pos_start_time
+        neg_ref.append((neg_start_time, neg_end_time))
+    
+        prev_pos_end_time   = curr_pos_end_time
+    
+    if prev_pos_end_time < soundscape_length:
+        neg_ref.append((prev_pos_end_time, soundscape_length))
+
+    return neg_ref
+
+   
+def load_neg_ref(ref_path, soundscape_length):
+    pos_ref = load_pos_ref(ref_path)
+
+    neg_ref = []
+    prev_pos_end_time   = 0
+    for (curr_pos_start_time, curr_pos_end_time) in pos_ref:
+        neg_start_time = prev_pos_end_time
+        neg_end_time   = curr_pos_start_time
+        neg_ref.append((neg_start_time, neg_end_time))
+    
+        prev_pos_end_time   = curr_pos_end_time
+    
+    if prev_pos_end_time < soundscape_length:
+        neg_ref.append((prev_pos_end_time, soundscape_length))
+
+    return neg_ref
+
+def load_timings_and_embeddings(base_dir, soundscape_basename, embedding_dim=1024):
+    file_path = os.path.join(base_dir, '{}.birdnet.embeddings.txt'.format(soundscape_basename))
+    timings = []
+    embeddings = []
     with open(file_path, 'r') as f:
         data = f.readlines()
         for d in data:
@@ -15,8 +65,9 @@ def load_timings_and_embeddings(file_path, embedding_dim=1024):
             assert(len(embedding) == len(values))
             for i, value in enumerate(values):
                 embedding[i] = float(value)
-            timings_and_embeddings.append((float(start_time), float(end_time), embedding))
-    return timings_and_embeddings
+            timings.append((float(start_time), float(end_time)))
+            embeddings.append(embedding)
+    return np.array(timings), np.array(embeddings)
 
 class FixedQueryActiveLearningDataset:
     def __init__(self, base_dir):
