473fc7ef46d2de8b1e840d21d1fa959e95058261
John Martinsson <john.martinsson@gmail.com>
2023-11-24 11:14:02 +0100

    Adds visualization functionality.

--- visualize.py
+++ visualize.py
@@ -5,6 +5,8 @@ from scipy.signal import find_peaks, peak_widths
 
 import matplotlib.pyplot as plt
 
+import evaluate
+import warnings
 import models
 import utils
 import datasets
@@ -14,24 +16,33 @@ import change_point_detection as cpd
 
 colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-def visualize_query_strategy(query_strategy, query_strategy_name, soundscape_basename, base_dir, n_queries=7, vis_probs=True, vis_queries=True, vis_threshold=True, vis_cpd=True, vis_label=True, vis_peaks=True, vis_true=True, savefile=None, noise_factor=0, normalize=True):
+def visualize_query_strategy(query_strategy, query_strategy_name, soundscape_basename, base_dir, n_queries=7, vis_probs=True, vis_queries=True, vis_threshold=True, vis_cpd=True, vis_label=True, vis_peaks=True, vis_true=True, vis_embs_label=False, savefile=None, noise_factor=0, normalize=True, emb_win_length=1.0):
     oracle = oracles.WeakLabelOracle(base_dir)
+
+    #print("###########################################")
+    #print(query_strategy_name)
+    #print("###########################################")
     
     soundscape_length = qs.get_soundscape_length(base_dir, soundscape_basename)
 
     timings, embeddings = datasets.load_timings_and_embeddings(base_dir, soundscape_basename)
     pred_probas = query_strategy.predict_probas(embeddings, noise_factor=noise_factor)
-    if query_strategy.fixed_queries or query_strategy.emb_cpd:
+    if query_strategy.fixed_queries or query_strategy.emb_cpd or query_strategy.opt_queries:
         pred_queries = query_strategy.predict_queries(base_dir, soundscape_basename, n_queries)
     else:
         pred_queries = models.queries_from_probas(pred_probas, timings, n_queries, soundscape_length)
         pred_queries = sorted(pred_queries, key=lambda x: x[0])
 
     pred_pos_events = oracle.pos_events_from_queries(pred_queries, soundscape_basename)
+
+    _, _, embs_label = evaluate.get_embeddings_3(pred_pos_events, base_dir, soundscape_basename, emb_win_length)
+
     ts_probas = np.mean(timings, axis=1)
 
     opt_queries = qs.optimal_query_strategy(base_dir, soundscape_basename, soundscape_length)
+    #print("opt qs: ", opt_queries)
     ref_pos_events = oracle.pos_events_from_queries(opt_queries, soundscape_basename)
+    #print("ref pos: ", ref_pos_events)
 
     # extract Mel spectrogram
     window_length = 0.025
@@ -74,6 +85,9 @@ def visualize_query_strategy(query_strategy, query_strategy_name, soundscape_bas
     peak_prominences = peaks[1]['prominences']
     peak_indices = sorted(utils.sort_by_rank(peak_prominences, peak_indices)[:n_queries-1])
 
+    if vis_embs_label:
+        ax[1].plot(ts_probas, embs_label, label='emb. label', color=colors[3])
+
     if vis_peaks:
         ax[1].plot(ts_probas[peak_indices], ds[peak_indices], "x", color="red", label='peaks')
 
@@ -106,7 +120,8 @@ def visualize_query_strategy(query_strategy, query_strategy_name, soundscape_bas
 
     # plot true event onsets and offsets
     if vis_true:
-        plot_events(ax[1], ref_pos_events, color='green', label='reference labels', ymax=0.9)
+        #print(ref_pos_events)
+        plot_events(ax[1], ref_pos_events, color=colors[2], label='reference labels', ymax=0.9)
         #plot_queries(ax[1], opt_queries, color='magenta', label='reference queries')
         #query_centers = [e - ((e - s) / 2) for (s, e) in opt_queries]
         #for idx_q_c, q_c in enumerate(query_centers):
@@ -117,7 +132,9 @@ def visualize_query_strategy(query_strategy, query_strategy_name, soundscape_bas
         plot_events(ax[1], pred_pos_events, color='magenta', label='annotated labels', ymax=0.95)
 
     if vis_queries:
-        assert len(pred_queries) == n_queries
+        assert len(pred_queries) <= n_queries
+        if len(pred_queries) < n_queries:
+            warnings.warn("not using all queries: {}".format(len(pred_queries)))
         plot_queries(ax[1], pred_queries, color='magenta', label='predicted queries')
 
         query_centers = [e - ((e - s) / 2) for (s, e) in pred_queries]
