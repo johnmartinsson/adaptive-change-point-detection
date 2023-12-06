35a85598743fc5360d27cc71fe0cde10ae641b2c
John Martinsson <john.martinsson@gmail.com>
2023-12-06 14:42:23 +0100

    Adds to gitignore.

--- generate_scaper_data_ME.sh
+++
@@ -1,6 +0,0 @@
-#python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=40
-#python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=30
-#python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME/Q/ --snr=20
-python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME_forest_path/Q/ --snr=10
-python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME_forest_path/Q/ --snr=0
-python generate_scaper_data.py --data_dir=/mnt/storage_1/datasets/dcase_bioacoustics_soundscapes/ME_forest_path/Q/ --snr=-10
