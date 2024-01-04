c9ec11ee537c7637e2b0c409caeab4cad8840e8a
John Martinsson <john.martinsson@gmail.com>
2024-01-04 12:55:03 +0100

    Add scripts for evaluation, model evaluation, data generation, and simulation

---
+++ scripts/evaluate_models.sh
@@ -0,0 +1,4 @@
+bash evaluate_all.sh $1 results/10_runs/$1/n_queries_7_fp_noise_0.0_fn_noise_0.0/ prototypical
+bash evaluate_all.sh $1 results/10_runs/$1/n_queries_7_fp_noise_0.0_fn_noise_0.0/ random_forest
+bash evaluate_all.sh $1 results/10_runs/$1/n_queries_7_fp_noise_0.0_fn_noise_0.0/ logistic_regression
+bash evaluate_all.sh $1 results/10_runs/$1/n_queries_7_fp_noise_0.0_fn_noise_0.0/ knn
