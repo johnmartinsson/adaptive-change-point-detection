fff2621d7d09933f3042442cd5e9f63c52ce38e8
John Martinsson <john.martinsson@gmail.com>
2024-01-04 13:26:46 +0100

    Update evaluate_all.sh script with additional parameters

--- scripts/evaluate_all.sh
+++ scripts/evaluate_all.sh
@@ -1,4 +1,4 @@
-python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=OPT --class_name=$1
-python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=ADP --class_name=$1
-python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=FIX --class_name=$1
-python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=CPD --class_name=$1
+python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=OPT --class_name=$1 --n_runs=10 --model_name=$3
+python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=ADP --class_name=$1 --n_runs=10 --model_name=$3
+python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=FIX --class_name=$1 --n_runs=10 --model_name=$3
+python evaluate.py --sim_dir=$2 --emb_win_length=1.0 --strategy_name=CPD --class_name=$1 --n_runs=10 --model_name=$3
