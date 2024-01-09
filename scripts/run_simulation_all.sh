ce86295ef443efe5e25ad7742b751b0d0889f8c8
John Martinsson <john.martinsson@gmail.com>
2024-01-09 14:17:02 +0100

    Delete unnecessary evaluation and simulation scripts

--- scripts/run_simulation_all.sh
+++
@@ -1,1 +0,0 @@
-python run_simulation.py --results_dir=results/10_runs_noisy_oracle_0.75_acc/ --name=$1 --class_name=$1 --n_soundscapes_budget=200 --n_queries_budget=7 --n_runs=10 --emb_win_length=1.0 --fp_noise=0.25 --fn_noise=0.25
--- scripts/sound_event_eval.sh
+++
@@ -1,8 +0,0 @@
-python sound_event_eval.py --class_name=baby --model_name=prototypical --sim_dir=results/10_runs_noisy_oracle_0.75_acc/baby/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
-python sound_event_eval.py --class_name=baby --model_name=mlp --sim_dir=results/10_runs_noisy_oracle_0.75_acc/baby/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
-
-python sound_event_eval.py --class_name=me --model_name=prototypical --sim_dir=results/10_runs_noisy_oracle_0.75_acc/me/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
-python sound_event_eval.py --class_name=me --model_name=mlp --sim_dir=results/10_runs_noisy_oracle_0.75_acc/me/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
-
-python sound_event_eval.py --class_name=dog --model_name=prototypical --sim_dir=results/10_runs_noisy_oracle_0.75_acc/dog/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
-python sound_event_eval.py --class_name=dog --model_name=mlp --sim_dir=results/10_runs_noisy_oracle_0.75_acc/dog/n_queries_7_fp_noise_0.25_fn_noise_0.25/ --t_collar=2.0 --n_runs=10
