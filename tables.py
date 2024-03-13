6a4c657d3c94612adba0647f432b68f01570751b
John Martinsson <john.martinsson@ri.se>
2024-03-13 09:37:22 +0100

    Remove LFS and update scripts to reproduce results.

--- tables.py
+++ tables.py
@@ -17,6 +17,7 @@ def main():
 
     conf = config.Config()
     conf.load_config_yaml(config_files[0])
+    conf.pretty_print()
 
 
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
