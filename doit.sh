7d572e074c263464cd0a5ad7443178f8ac444605
John Martinsson <john.martinsson@gmail.com>
2024-03-14 18:22:34 +0100

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,8 +1,10 @@
+# download results and data
 wget https://zenodo.org/records/10811797/files/results_eusipco_2024.zip
 wget https://zenodo.org/records/10811797/files/data.zip
 unzip data.zip
 unzip results_eusipco_2024.zip
 
+# setup environment
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip3 install -r requirements.txt
 
