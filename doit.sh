f8156abe2611da14261f244178680bcc5ce1fbad
John Martinsson <john.martinsson@gmail.com>
2024-03-14 18:11:34 +0100

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -5,3 +5,9 @@ unzip results_eusipco_2024.zip
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip3 install -r requirements.txt
+
+# produces all tables in the paper
+python src/tables.py
+
+# produces all figures in the paper
+python src/figures.py
